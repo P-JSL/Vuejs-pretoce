2090a2ab75dd4c9b8aba22422bfc32f1988441c6
P-JSL <althsuwpfl@gmail.com>
2022-06-20 17:17:09 +0900

    vue-cli

---
+++ petstore/src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <header>
+      <div class="navbar navbar-default">
+        <div class="navbar-header">
+          <h1><router-link to="{name : 'iMain'}">{{sitename}}</router-link></h1> <!--iMain 경로-->
+        </div>
+      </div>
+      <div class="nav navbar-nav navbar-right cart">
+        <router-link active-class="active" class="btn btn-default btn-lg" tag="button" to="{name : 'Form'}">
+          <span class="glyphicon glyphicon-shopping-cart">
+            {{cartItemCount}}
+          </span>
+          체크아웃
+        </router-link>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header.vue",
+  data(){
+    return {
+        sitename : "Vue.js 애완용품샵",
+      }
+    },
+  props : ['cartItemCount'],
+  methods: {
+    showCheckout(){
+      this.$router.push({name:'Form'});
+    }
+  }
+}
+</script>
+
+<style scoped>
+a{
+  text-decoration: none;
+  color:black;
+}
+</style>
