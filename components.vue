99ae683992355f14d4fc74c44de893175711471b
P-JSL <althsuwpfl@gmail.com>
2022-06-20 15:38:10 +0900

    연습

---
+++ components.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name:'hello',
+  data(){
+    return {
+      msg : 'Vue.js앱에 오신걸 환영 합니다.'
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
